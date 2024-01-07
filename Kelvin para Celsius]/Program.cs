b61fe63657dc9c07e3a412ac3e80e10e28c6e849
morais007 <emuladorideal74@gmail.com>
2024-01-07 19:06:25 -0300

    Adicionar arquivos de projeto.

---
+++ Kelvin para Celsius]/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kelvin_para_Celsius_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kelvin apara Grau Celsius 37 K − 273,15 
+
+            double kelvin, celsius;
+
+            Console.WriteLine("Transformar Kelvin para Grau Celsius.");
+            Console.WriteLine("Formula: K − 273,15 = -236,1 °C\r\n");
+
+            Console.Write("Digite Uma temperatura: ");
+            kelvin = double.Parse(Console.ReadLine());
+
+            celsius= kelvin - 273.15;
+
+            Console.WriteLine($"Portanto, {kelvin} Kelvin equivalem a {celsius} graus Celsius.");
+
+            Console.ReadKey();
+            
+
+        }
+    }
+}
